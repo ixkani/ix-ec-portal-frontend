8b78c6d272becb760c64e5e3cf6bd8695aaf0e30
kaniazhagan <kaniazhagan.kasi@ionixxtech.com>
2018-03-05 11:45:48 +0530

    changes

--- node_modules/@ng-idle/core/src/documentinterruptsource.d.ts
+++
@@ -1,5 +0,0 @@
-import { EventTargetInterruptSource } from './eventtargetinterruptsource';
-export declare class DocumentInterruptSource extends EventTargetInterruptSource {
-    constructor(events: string, throttleDelay?: number);
-    filterEvent(event: any): boolean;
-}
--- node_modules/@ng-idle/core/src/windowinterruptsource.d.ts
+++
@@ -1,4 +0,0 @@
-import { EventTargetInterruptSource } from './eventtargetinterruptsource';
-export declare class WindowInterruptSource extends EventTargetInterruptSource {
-    constructor(events: string, throttleDelay?: number);
-}
