edc90ded7a4d62ce7674d47e14f49b347f4bad36
kaniazhagan <kaniazhagan.kasi@ionixxtech.com>
2018-03-05 11:53:49 +0530

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,6 +13,6 @@ export const environment = {
     client_id:'Q0W1osEOriGM0rwlt7ZBE2ArpDAuczZyDxUmQyx6neVBbU4lkI',
     client_secret:'RPHtn6oWjCsQuwYyi5j0Jh2M8hl93LsYk934pR81',
     token_url: 'http://ec2-52-207-28-114.compute-1.amazonaws.com:8008/o/token/',
-    notification_url: 'http://ec2-52-207-28-114.compute-1.amazonaws.com
+    notification_url: 'http://ec2-52-207-28-114.compute-1.amazonaws.com'
   }
 };
